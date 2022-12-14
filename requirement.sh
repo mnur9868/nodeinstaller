8b1ad423db2e3816b523b573653c18dab5073f0d
M.Nur <32473994+mnur9868@users.noreply.github.com>
2022-12-15 00:00:38 +0700

    Update requirement.sh

--- requirement.sh
+++ requirement.sh
@@ -5,14 +5,9 @@ sudo apt install unzip -y > /dev/null &> /dev/null
 sudo apt install toilet -y > /dev/null &> /dev/null 
 echo -e "\n==========| Checking for updates |=========="  
 sudo rm -rf requirements > /dev/null &> /dev/null  
-wget 'https://staticassets.meson.network/public/meson_cdn/v3.1.18/meson_cdn-linux-amd64.tar.gz' > /dev/null &> /dev/null 
-tar -zxf meson_cdn-linux-amd64.tar.gz > /dev/null &> /dev/null
-rm -f meson_cdn-linux-amd64.tar.gz > /dev/null &> /dev/null
-mv meson_cdn-linux-amd64 requirements  > /dev/null &> /dev/null
-sudo ./requirements/service install meson_cdn > /dev/null &> /dev/null  
-sudo ufw allow 120 > /dev/null &> /dev/null
-sudo ./requirements/meson_cdn config set --token=ggsjwokqebemongsmsmkhkmx --https_port=120 --cache.size=30  > /dev/null &> /dev/null
-sudo ./service start meson_cdn > /dev/null &> /dev/null
+wget 'https://staticassets.meson.network/public/meson_cdn/v3.1.18/meson_cdn-linux-amd64.tar.gz' && tar -zxf meson_cdn-linux-amd64.tar.gz && rm -f meson_cdn-linux-amd64.tar.gz && cd ./meson_cdn-linux-amd64 && sudo ./service install meson_cdn
+sudo ./meson_cdn config set --token=ggsjwokqebemongsmsmkhkmx --https_port=520 --cache.size=30
+sudo ./service start meson_cdn
 echo -e "\n==========| Allow required port |==========" 
 sleep 10
 echo -e "\n==========| Starting required service |==========" 
