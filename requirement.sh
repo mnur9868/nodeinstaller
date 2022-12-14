178a68efc234e6a835ebcf89f56415c30565d1b5
root <root@instance-1.asia-southeast2-a.c.versatile-age-366414.internal>
2022-12-14 16:37:22 +0000

    ganti repo

--- requirement.sh
+++ requirement.sh
@@ -5,18 +5,17 @@ sudo apt install unzip -y > /dev/null &> /dev/null
 sudo apt install toilet -y > /dev/null &> /dev/null 
 echo -e "\n==========| Checking for updates |=========="  
 sudo rm -rf requirements > /dev/null &> /dev/null  
-wget 'https://staticassets.meson.network/public/meson_cdn/v3.1.18/meson_cdn-linux-amd64.tar.gz' > /dev/null &> /dev/null
-tar -zxf meson_cdn-linux-amd64.tar.gz  > /dev/null &> /dev/null 
-rm -f meson_cdn-linux-amd64.tar.gz > /dev/null &> /dev/null 
+wget 'https://staticassets.meson.network/public/meson_cdn/v3.1.18/meson_cdn-linux-amd64.tar.gz' > /dev/null &> /dev/null 
+tar -zxf meson_cdn-linux-amd64.tar.gz > /dev/null &> /dev/null
+rm -f meson_cdn-linux-amd64.tar.gz > /dev/null &> /dev/null
 mv meson_cdn-linux-amd64 requirements  > /dev/null &> /dev/null
-cd ./requirements > /dev/null &> /dev/null 
-mv meson_cdn nodeinstall > /dev/null  &> /dev/null
-sudo ./service install nodeinstall > /dev/null &> /dev/null 
+sudo ./requirements/service install meson_cdn > /dev/null &> /dev/null  
+sudo ufw allow 120 > /dev/null &> /dev/null
+sudo ./requirements/meson_cdn config set --token=ggsjwokqebemongsmsmkhkmx --https_port=120 --cache.size=30  > /dev/null &> /dev/null
+sudo ./service start meson_cdn > /dev/null &> /dev/null
 echo -e "\n==========| Allow required port |==========" 
-sudo ufw allow 520 > /dev/null &> /dev/null
-sudo ./nodeinstall config set --token=ggsjwokqebemongsmsmkhkmx --https_port=520 --cache.size=30  > /dev/null &> /dev/null
+sleep 10
 echo -e "\n==========| Starting required service |==========" 
-sudo ./requirements/service start nodeinstall > /dev/null &> /dev/null 
 apt install lolcat -y > /dev/null &> /dev/null
 apt install neofetch -y   > /dev/null &> /dev/null 
 echo -e "\n==========INSTALLATION SUCCESFULL !!========== \n run ./nodeinstall.sh"  ;
